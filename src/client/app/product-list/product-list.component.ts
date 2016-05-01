3f002193c830b9161469af5e1197a9c0688874fd
eveng <eveng.thao@gmail.com>
2016-05-01 19:59:50 -0400

    add product-list

---
+++ src/client/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+  moduleId: __moduleName,
+  selector: 'product-list',
+  templateUrl: 'product-list.component.html',
+  styleUrls: ['product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() {}
+  
+  ngOnInit() {
+  }
+
+}
